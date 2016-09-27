1da3f3e83d6e9a991e8d52ce8b6f8b20bf92d19c
LMCMLJ <mjcalvey2@gmail.com>
2016-09-27 16:21:41 +0100

    Adds test for release_bike method, passes test with new method

--- spec/boris_bikes_spec.rb
+++ spec/boris_bikes_spec.rb
@@ -2,4 +2,8 @@ require 'boris_bikes'
 
 describe DockingStation do
 
+  it 'releases bike' do
+    docking_station = DockingStation.new
+    expect(docking_station).to respond_to :release_bike
+  end
 end
